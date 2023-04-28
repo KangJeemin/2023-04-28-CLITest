77007079573f3d7597c9eb4634aeafcb8a67723d
KangJeemin <wlals1234560@gmail.com>
2023-04-28 11:09:29 +0900

    option()안에 첫번째인자중 쉼표 전은 사용자가 입력할 값, 쉼표 다음은 담길 객체의 키값 ,데이터 타입 이였음!!!!

--- clitest.js
+++ clitest.js
@@ -6,12 +6,12 @@ program
   .usage(['의 사용법 입니다.'])
 program
   .option('-n,--name <name>','file name.html이 만들어질 예정','강지민.html')
-  .option('-c, --compress','root태그 생성 여부','예')
-  .option('-d,--directory[path]','생성 경로를 입력하세요','.')
+  .option('-c,--compress','root태그 생성 여부','예')
+  .option('-d,--directory <name>','생성 경로를 입력하세요','./')
   .parse()
   
 
-  console.log(program.opts().n);
+  console.log(program.opts());
   console.log(program.opts().name);
   console.log(program.opts().compress);
   console.log(program.opts().directory);
