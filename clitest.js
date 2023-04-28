6599f105481a755a9cba4a11834a1a7eba3951d0
KangJeemin <wlals1234560@gmail.com>
2023-04-28 11:02:18 +0900

    option()안에 있는 첫번째 인자-1은  커맨드에 입력하는 텍스트이고, 첫번째 인자 -2는 program에 담길 객체

---
+++ clitest.js
@@ -0,0 +1,22 @@
+import {Command} from 'commander'
+const program = new Command();
+
+program
+  .version('0.0.1','-v,--version')
+  .usage(['의 사용법 입니다.'])
+program
+  .option('-n,--name <name>','file name.html이 만들어질 예정','강지민.html')
+  .option('-c, --compress','root태그 생성 여부','예')
+  .option('-d,--directory[path]','생성 경로를 입력하세요','.')
+  .parse()
+  
+
+  console.log(program.opts().n);
+  console.log(program.opts().name);
+  console.log(program.opts().compress);
+  console.log(program.opts().directory);
+
+
+
+
+
